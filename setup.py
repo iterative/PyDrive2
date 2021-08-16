abe765506c83546961818118f26fc7479b0e14e7
Ruslan Kuprieiev <kupruser@gmail.com>
2021-08-16 13:42:32 +0300

    pydrive2: bump to 1.9.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if sys.version_info >= (3, 6):
 
 setup(
     name="PyDrive2",
-    version="1.9.0",
+    version="1.9.1",
     author="JunYoung Gwak",
     author_email="jgwak@dreamylab.com",
     maintainer="DVC team",
