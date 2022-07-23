6974ba8a05836968d6ccf063d4c3ed5985255714
Ivan Shcheklein <shcheklein@gmail.com>
2022-07-23 16:55:16 -0700

    remove six

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
     long_description_content_type="text/x-rst",
     install_requires=[
         "google-api-python-client >= 1.12.5",
-        "six >= 1.13.0",
         "oauth2client >= 4.0.0",
         "PyYAML >= 3.0",
         "pyOpenSSL >= 19.1.0",
