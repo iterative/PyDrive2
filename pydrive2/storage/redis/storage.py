053e864f755121a53039499a3b5f7871b57949a0
파링 <paring@pikokr.dev>
2023-03-21 12:17:00 +0900

    feat: lock for redis storage

--- pydrive2/storage/redis/storage.py
+++ pydrive2/storage/redis/storage.py
@@ -7,21 +7,49 @@ class RedisStorage(client.Storage):
         super(RedisStorage, self).__init__()
         self.redis = redis
         self.key = key
+        self._lock = redis.lock(key + '_lock')
 
     def get(self):
-        serialized = self.redis.get(self.key)
+        return self.locked_get()
 
-        if serialized is None:
-            return None
-
-        credentials = client.OAuth2Credentials.from_json(serialized)
-        credentials.set_store(self)
-
-        return credentials
-
-    def put(self, credentials):
-        serialized = credentials.to_json()
-        self.redis.set(self.key, serialized)
+    def put(self, credentials: client.Credentials):
+        self.locked_put(credentials)
 
     def delete(self):
-        self.redis.delete(self.key)
+        self.delete()
+
+    # Locked operations
+
+    def acquire_lock(self):
+        self._lock.acquire(blocking=True)
+
+    def release_lock(self):
+        self._lock.release()
+
+    def locked_get(self):
+        self.acquire_lock()
+
+        try:
+            serialized = self.redis.get(self.key)
+
+            credentials = client.OAuth2Credentials.from_json(serialized)
+            credentials.set_store(self)
+
+            return credentials
+        finally:
+            self.release_lock()
+
+    def locked_put(self, credentials: client.Credentials):
+        self.acquire_lock()
+        try:
+            serialized = credentials.to_json()
+            self.redis.set(self.key, serialized)
+        finally:
+            self.release_lock()
+
+    def locked_delete(self):
+        self.acquire_lock()
+        try:
+            self.redis.delete(self.key)
+        finally:
+            self.release_lock()
