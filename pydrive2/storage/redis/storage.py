dbbadf419edd3db024c2d665d90af9647ecdd311
파링 <paring@pikokr.dev>
2023-03-21 12:32:47 +0900

    fix: deadlock

--- pydrive2/storage/redis/storage.py
+++ pydrive2/storage/redis/storage.py
@@ -4,21 +4,9 @@ from redis import Redis
 
 class RedisStorage(client.Storage):
     def __init__(self, redis: Redis, key: str = "pydrive_oauth"):
-        super(RedisStorage, self).__init__()
+        super(RedisStorage, self).__init__(lock=redis.lock(key + '_lock'))
         self.redis = redis
         self.key = key
-        self._lock = redis.lock(key + '_lock')
-
-    def get(self):
-        return self.locked_get()
-
-    def put(self, credentials: client.Credentials):
-        self.locked_put(credentials)
-
-    def delete(self):
-        self.delete()
-
-    # Locked operations
 
     def acquire_lock(self):
         self._lock.acquire(blocking=True)
@@ -27,29 +15,16 @@ class RedisStorage(client.Storage):
         self._lock.release()
 
     def locked_get(self):
-        self.acquire_lock()
+        serialized = self.redis.get(self.key)
 
-        try:
-            serialized = self.redis.get(self.key)
+        credentials = client.OAuth2Credentials.from_json(serialized)
+        credentials.set_store(self)
 
-            credentials = client.OAuth2Credentials.from_json(serialized)
-            credentials.set_store(self)
-
-            return credentials
-        finally:
-            self.release_lock()
+        return credentials
 
     def locked_put(self, credentials: client.Credentials):
-        self.acquire_lock()
-        try:
-            serialized = credentials.to_json()
-            self.redis.set(self.key, serialized)
-        finally:
-            self.release_lock()
+        serialized = credentials.to_json()
+        self.redis.set(self.key, serialized)
 
     def locked_delete(self):
-        self.acquire_lock()
-        try:
-            self.redis.delete(self.key)
-        finally:
-            self.release_lock()
+        self.redis.delete(self.key)
