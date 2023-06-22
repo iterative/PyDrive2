ad45ca692d9baaaa30e88fef6eb8eb3f9dcb3f40
skshetry <18718008+skshetry@users.noreply.github.com>
2023-06-22 18:34:15 +0545

    drop Python 3.7 support (#288)
    * drop Python 3.7 support
    
    * Update tox.ini

--- setup.py
+++ setup.py
@@ -50,14 +50,14 @@ setup(
         ],
         "tests": tests_requirements,
     },
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     classifiers=[
         "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     entry_points={
         "pyinstaller40": [
